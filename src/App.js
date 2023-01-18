04c72ad7f2d405b30047d2a4ffdab6f5e82f4741
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-18 04:13:48 -0800

    designing a hero section i.e header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import { TopBar } from "./components";
+import Home from "./pages/home/Home";
 
 
 function App() {
   return (
     <div>
        <TopBar />
+       <Home />
     </div>
   );
 }
---
+++ src/pages/home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import "./home.css"
+export default function Home() {
+  return (
+    <div className='home'>
+      <Header />
+      Home Page
+    </div>
+  )
+}
