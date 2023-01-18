879bdd5ad311a3c31a7e18210b8bedd9029f743a
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-18 04:43:29 -0800

    creating a sidebar component

---
+++ src/components/sideBar/Sidebar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import AboutImg from "../../assets/about-img.jpg";
+import "./sidebar.css";
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img src={AboutImg} alt="about-img" />
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium
+          nobis recusandae quam exercitationem possimus eveniet aspernatur, id
+          hic! Fugiat assumenda aut nemo adipisci ullam dicta, deserunt libero
+          porro sequi architecto?
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">Life</li>
+          <li className="sidebarListItem">Music</li>
+          <li className="sidebarListItem">Style</li>
+          <li className="sidebarListItem">Tech</li>
+          <li className="sidebarListItem">Sport</li>
+          <li className="sidebarListItem">Cinema</li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import Header from '../../components/header/Header'
-import "./home.css"
+import React from "react";
+import Header from "../../components/header/Header";
+import Sidebar from "../../components/sideBar/Sidebar";
+import Posts from "../../posts/Posts";
+import "./home.css";
 export default function Home() {
   return (
-    <div className='home'>
+    <>
       <Header />
-      Home Page
-    </div>
-  )
+      <div className="home">
+         <Posts />
+         <Sidebar />
+      </div>
+    </>
+  );
 }
