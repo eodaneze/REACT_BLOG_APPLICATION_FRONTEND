9dd5a2321c37f9d5dd893f96a2a2b39b4123f180
Daniel Ezeali <ezealiokechukwu1999@gmail.com>
2023-01-19 03:30:44 -0800

    creating a write post component

---
+++ src/components/pages/write/Write.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './write.css'
+import writeImg from "../../../assets/blog-1.avif"
+const Write = () => {
+  return (
+    <div className='write'>
+        <img className='writeImg' src={writeImg} alt="" />
+        <form className="writeForm">
+            <div className="writeFormGroup">
+                <label htmlFor="fileInput">
+                    <i className="fas fa-plus writeIcon"></i>
+                </label>
+                 <input type="file" id="fileInput" style={{display: "none"}} />
+                 <input type="text" placeholder='Title' className='writeInput' autoFocus={true}/>
+            </div>
+            <div className="writeFormGroup">
+                 <textarea placeholder='Tell your story...' type='text' className='writeInput writeText'></textarea>
+            </div>
+            <button className="writeSubmit">Publish</button>
+        </form>
+    </div>
+  )
+}
+
+export default Write
